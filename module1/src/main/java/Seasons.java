8b0b53a5eee9552f667c9fcd3fe556d9499bf44a
Roman Hrynchuk <rhrynch@softserveinc.com>
2019-04-02 00:18:32 +0300

    season reader

---
+++ module1/src/main/java/Seasons.java
@@ -0,0 +1,11 @@
+/**
+ * Created by romanhrynchuk on 4/1/19.
+ */
+public enum Seasons {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN
+}
+
+
---
+++ module1/src/test/java/SeasonReader.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by romanhrynchuk on 4/1/19.
+ */
+public class SeasonReader {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        System.out.println("Input your month:");
+        String month = br.readLine();
+
+
+        switch (month.toUpperCase()) {
+            case "DECEMBER":
+            case "JANUARY":
+            case "FEBRUARY":
+                System.out.println(month + " is in " + Seasons.WINTER);
+                break;
+
+            case "MARCH":
+            case "APRIL":
+            case "MAY":
+                System.out.println(month + " is in " + Seasons.SPRING);
+                break;
+
+            case "JUNE":
+            case "JULY":
+            case "AUGUST":
+                System.out.println(month +" is in "+ Seasons.SUMMER);
+                break;
+
+            case "SEPTEMBER":
+            case "OCTOBER":
+            case "NOVEMBER":
+                System.out.println(month +" is in "+ Seasons.AUTUMN);
+                break;
+
+            default:
+                System.out.println("Hey, are you sure such month even exists?");
+        }
+
+    }
+}
